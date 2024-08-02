d03345c5d06641d9231f67fe88136467b3f69bc6
Yusuke Izawa <me@yizawa.com>
2024-08-02 12:17:43 +0900

    Fix ssa.ml

--- ssa.ml
+++ ssa.ml
@@ -2,55 +2,94 @@ open Syntax
 
 module M = Map.Make(String)
 
-let m : (string * int) list ref = ref []
-
 let id_num = ref (-1)
 
-let gen_id_num () =
-  incr id_num; !id_num
+let gen_id s =
+  incr id_num;
+  s ^ "." ^ (string_of_int !id_num)
 
-let rec assign_id_arith (arith : Syntax.a) =
+let find_or_gen_id (x : id) (env : id M.t) =
+  try
+    M.find x env, env
+  with Not_found ->
+    let id' = gen_id x in
+    let env = M.add x id' env in
+    id', env
+
+
+let rec assign_id_arith (arith : Syntax.a) (env : id M.t) =
   match arith with
-  | Num n -> Num n
+  | Num n -> Num n, env
   | Var (id) ->
-    (match List.assoc_opt id !m with
-     | Some id_num -> Var (id ^ (string_of_int id_num))
-     | None ->
-       let id_num = gen_id_num () in
-       m := (id, id_num) :: !m;
-       Var (id ^ (string_of_int id_num)))
+    let id', env = find_or_gen_id id env in
+    Var (id'), env
   | Add (lhs, rhs) ->
-    Add (assign_id_arith lhs, assign_id_arith rhs)
+    let lhs, _ = assign_id_arith lhs env in
+    let rhs, _ = assign_id_arith rhs env  in
+    Add (lhs, rhs), env
   | Sub (lhs, rhs) ->
-    Sub (assign_id_arith lhs, assign_id_arith rhs)
+    let lhs, _ = assign_id_arith lhs env in
+    let rhs, _ = assign_id_arith rhs env  in
+    Sub (lhs, rhs), env
   | Mul (lhs, rhs) ->
-    Mul (assign_id_arith lhs, assign_id_arith rhs)
+    let lhs, _ = assign_id_arith lhs env in
+    let rhs, _ = assign_id_arith rhs env  in
+    Mul (lhs, rhs), env
   | Div (lhs, rhs) ->
-    Div (assign_id_arith lhs, assign_id_arith rhs)
+    let lhs, _ = assign_id_arith lhs env in
+    let rhs, _ = assign_id_arith rhs env  in
+    Div (lhs, rhs), env
 
 
-let rec assign_id_pred (pred : Syntax.p) =
+let rec assign_id_pred (pred : Syntax.p) (env : id M.t) =
   match pred with
-  | True | False | Not _ | And _ | Or _ -> pred
-  | LT (a1, a2) -> LT (assign_id_arith a1, assign_id_arith a2)
-  | LE (a1, a2) -> LE (assign_id_arith a1, assign_id_arith a2)
-  | GT (a1, a2) -> GT (assign_id_arith a1, assign_id_arith a2)
-  | GE (a1, a2) -> GE (assign_id_arith a1, assign_id_arith a2)
-  | EQ (a1, a2) -> EQ (assign_id_arith a1, assign_id_arith a2)
+  | True | False | Not _ | And _ | Or _ -> pred, env
+  | LT (a1, a2) ->
+    let a1, env = assign_id_arith a1 env in
+    let a2, env = assign_id_arith a2 env in
+    LT (a1, a2), env
+  | LE (a1, a2) ->
+    let a1, env = assign_id_arith a1 env in
+    let a2, env = assign_id_arith a2 env in
+    LE (a1, a2), env
+  | GT (a1, a2) ->
+    let a1, env = assign_id_arith a1 env in
+    let a2, env = assign_id_arith a2 env in
+    GT (a1, a2), env
+  | GE (a1, a2) ->
+    let a1, env = assign_id_arith a1 env in
+    let a2, env = assign_id_arith a2 env in
+    GE (a1, a2), env
+  | EQ (a1, a2) ->
+    let a1, env = assign_id_arith a1 env in
+    let a2, env = assign_id_arith a2 env in
+    EQ (a1, a2), env
 
-
-let rec assign_id_statement (stmt : Syntax.s) =
+let rec assign_id_statement (stmt : Syntax.s) (env : id M.t) =
   match stmt with
   | Assign (id, a) ->
-    let a = assign_id_arith a in
-    let id_num = gen_id_num () in
-    m := (id, id_num) :: !m;
-    Assign (id ^ (string_of_int id_num), a)
-  | Skip -> Skip
-  | Block (s) -> Block (assign_id_statement s)
+    let a', env = assign_id_arith a env in
+    let id' = gen_id id in
+    let env = M.add id id' env in
+    Assign (id', a'), env
+  | Skip -> Skip, env
+  | Block (s) ->
+    let s, env = assign_id_statement s env in
+    Block (s), env
   | Seq (s1, s2) ->
-    let s1 = assign_id_statement s1 in
-    let s2 = assign_id_statement s2 in
-    Seq (s1, s2)
+    let s1, env = assign_id_statement s1 env in
+    let s2, env = assign_id_statement s2 env in
+    Seq (s1, s2), env
   | While (p, s) ->
-    While (assign_id_pred p, assign_id_statement s)
+    let p', env = assign_id_pred p env in
+    let s', env = assign_id_statement s env in
+    While (p', s'), env
+  | If (cond, s1, s2) ->
+    let c, env = assign_id_pred cond env in
+    let s1, env = assign_id_statement s1 env in
+    let s2, env= assign_id_statement s2 env in
+    If (c, s1, s2), env
+
+let assign_id stmt =
+  let stmt, _ = assign_id_statement stmt M.empty in
+  stmt
