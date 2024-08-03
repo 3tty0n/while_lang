c5cdb6616da4e7daf60e75ce3ab2f024cf937564
Yusuke Izawa <me@yizawa.com>
2024-08-03 18:31:45 +0900

    Fix while_lang.ml

--- while_lang.ml
+++ while_lang.ml
@@ -2,26 +2,25 @@ let compile_pycode ic =
   let l = Lexing.from_channel ic in
   let oc = open_out ("out.pyc") in
   Assemble_pyc.assemble oc
-    (Emit_pyc.compile_and_create_pycode
-       (Parser.start Lexer.token l))
-
-let string_pycode s =
-  let l = Lexing.from_string s in
-  let oc = open_out ("out.pyc") in
-  Assemble_pyc.assemble oc
-    (Emit_pyc.compile_and_create_pycode
+    (Virtual_pyc.compile_and_create_pycode
        (Parser.start Lexer.token l))
 
 let print_opcode s =
   let l = Lexing.from_string s in
-  let opcode = Emit_pyc.compile_pyc
+  let opcode = Virtual_pyc.compile_pyc
       (Parser.start Lexer.token l) in
-  List.iter Emit_pyc.print_pyc opcode
+  Virtual_pyc.print_pyc_list opcode
 
-let test_pycode s =
-  string_pycode s
+let test_pycode fname s =
+  let l = Lexing.from_string s in
+  let oc = open_out fname in
+  Assemble_pyc.assemble oc
+    (Virtual_pyc.compile_and_create_pycode
+       (Parser.start Lexer.token l))
+
+let test () =
+  test_pycode "assign.pyc" "i := 1; i := i + 1; print i;";
+  test_pycode "loop.pyc" "i := 1; while i < 10 do begin i := i + 1; end; print i;"
 
 let () =
-  (* test "i := 1; a := 1; begin a := a * 2; i := i + 1; end;"; *)
-  print_opcode "i := 1; i := i + 1; print i;";
-  test_pycode "i := 1; i := i + 1; print i;"
+  test ()
